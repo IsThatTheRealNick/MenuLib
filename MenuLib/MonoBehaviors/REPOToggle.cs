4f9dc83a58a2e449324ef2ebdcb69cab92353961
Nick <nickkisbetterthanyou@gmail.com>
2025-03-21 18:43:33 -0400

    Fixed sliders

--- MenuLib/MonoBehaviors/REPOToggle.cs
+++ MenuLib/MonoBehaviors/REPOToggle.cs
@@ -8,7 +8,7 @@ namespace MenuLib.MonoBehaviors;
 
 public sealed class REPOToggle : MonoBehaviour, IREPOElement
 {
-    private static readonly Vector3 leftPosition = new(37.8f, 12.3f), leftScale = new(73f, 22f, 1f), rightPosition = new(112.644f, 12.3f), rightScale = new(74f, 22f, 1f);
+    private static readonly Vector3 leftPosition = new(137.8f, 12.3f), leftScale = new(73f, 22f, 1f), rightPosition = new(212.644f, 12.3f), rightScale = new(74f, 22f, 1f);
 
     public RectTransform rectTransform { get; private set; }
     public TextMeshProUGUI labelTMP, leftButtonTMP, rightButtonTMP;
@@ -22,13 +22,10 @@ public sealed class REPOToggle : MonoBehaviour, IREPOElement
     
     public void SetState(bool newState, bool invokeCallback)
     {
-        if (state == newState)
-            return;
-        
         targetPosition = newState ? leftPosition : rightPosition;
         targetScale = newState ? leftScale : rightScale;
         
-        if (invokeCallback)
+        if (invokeCallback && state != newState)
             onToggle?.Invoke(newState);
         
         state = newState;
@@ -40,16 +37,54 @@ public sealed class REPOToggle : MonoBehaviour, IREPOElement
         labelTMP = GetComponentInChildren<TextMeshProUGUI>();
         optionBox = (RectTransform) transform.Find("Option Box");
         optionBoxBehind = (RectTransform) transform.Find("Option Box Behind");
+
+        var background = transform.Find("SliderBG");
+        var backgroundLocalPosition = background.localPosition;
+        backgroundLocalPosition.x += 100;
+        background.localPosition = backgroundLocalPosition;
+
+        var labelSizeDelta = labelTMP.rectTransform.sizeDelta;
+        labelSizeDelta.x -= 4;
+        labelTMP.rectTransform.sizeDelta = labelSizeDelta;
+        
+        var labelLocalPosition = labelTMP.rectTransform.localPosition;
+        labelLocalPosition.x += 100;
+        labelTMP.rectTransform.localPosition = labelLocalPosition;
+        
+        var outline = transform.Find("RawImage");
+        var outlineLocalPosition = outline.localPosition;
+        outlineLocalPosition.x += 100;
+        outline.localPosition = outlineLocalPosition;
+        
+        var fill = transform.Find("RawImage (1)");
+        var fillLocalPosition = fill.localPosition;
+        fillLocalPosition.x += 100;
+        fill.localPosition = fillLocalPosition;
+        
+        var separator = transform.Find("RawImage (2)");
+        var separatorLocalPosition = separator.localPosition;
+        separatorLocalPosition.x += 100;
+        separator.localPosition = separatorLocalPosition;
         
         var buttons = GetComponentsInChildren<Button>();
         
         var leftButton = buttons[0];
+
+        var leftButtonLocalPosition = leftButton.transform.localPosition;
+        leftButtonLocalPosition.x += 100;
+        leftButton.transform.localPosition = leftButtonLocalPosition;
+        
         leftButton.onClick = new Button.ButtonClickedEvent();
         leftButton.onClick.AddListener(() => SetState(true, true));
         
         leftButtonTMP = leftButton.GetComponentInChildren<TextMeshProUGUI>();
         
         var rightButton = buttons[1];
+        
+        var rightButtonLocalPosition = rightButton.transform.localPosition;
+        rightButtonLocalPosition.x += 100;
+        rightButton.transform.localPosition = rightButtonLocalPosition;
+        
         rightButton.onClick = new Button.ButtonClickedEvent();
         rightButton.onClick.AddListener(() => SetState(false, true));
         rightButtonTMP = rightButton.GetComponentInChildren<TextMeshProUGUI>();
