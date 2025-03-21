2dc4af795553885efd4e50c5d88556fa02d13814
Nick <nickkisbetterthanyou@gmail.com>
2025-03-21 16:50:55 -0400

    Made labels smaller

--- MenuLib/MonoBehaviors/REPOLabel.cs
+++ MenuLib/MonoBehaviors/REPOLabel.cs
@@ -13,10 +13,10 @@ public sealed class REPOLabel : MonoBehaviour
         rectTransform = GetComponent<RectTransform>();
         labelTMP = GetComponentInChildren<TextMeshProUGUI>();
 
-        rectTransform.pivot = Vector2.zero;
-        labelTMP.rectTransform.pivot = new Vector2(0f, 0.5f);
-        labelTMP.rectTransform.sizeDelta = rectTransform.sizeDelta = new Vector2(200f, 40f);
-
+        labelTMP.rectTransform.pivot = rectTransform.pivot = Vector2.zero;
+        labelTMP.rectTransform.sizeDelta = rectTransform.sizeDelta = new Vector2(200f, 30f);
+        
+        labelTMP.fontSize = 30;
         labelTMP.enableWordWrapping = labelTMP.enableAutoSizing = false;
         labelTMP.alignment = TextAlignmentOptions.Left;
         labelTMP.margin = Vector4.zero;
